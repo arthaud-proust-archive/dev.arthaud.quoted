2382e75ed52e7821633882ed36cadadc5a694d9d
Arthaud Proust <arthaudp33@gmail.com>
2020-11-12 21:45:54 +0100

    fix meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <?php 
 
-$description = View::hasSection('description')?View::getSection('description'):'Read, write and share quotes around the world.';
+$description = View::hasSection('description')?View::getSection('description'):'Read, write and share quoted around the world.';
 $title = View::hasSection('title')?View::getSection('title').' - '.config('app.name', 'Quotes'):'Quotes';
 ?>
 
@@ -20,8 +20,8 @@ $title = View::hasSection('title')?View::getSection('title').' - '.config('app.n
     <link href="{{ asset('css/mobile-app.css') }}" rel="stylesheet" media="only screen and (max-width: 768px)">
     <link href="{{ asset('css/large-app.css') }}" rel="stylesheet" media="only screen and (min-width: 768px)">
 
-    <link rel="alternate" hreflang="fr-fr" href="https://fr.quotes.arthaud.dev" />
-    <link rel="alternate" hreflang="en-gb" href="https://en.quotes.arthaud.dev" />
+    <link rel="alternate" hreflang="fr-fr" href="https://fr.quoted.arthaud.dev" />
+    <link rel="alternate" hreflang="en-gb" href="https://en.quoted.arthaud.dev" />
 
 
     <!-- Scripts -->
@@ -38,28 +38,28 @@ $title = View::hasSection('title')?View::getSection('title').' - '.config('app.n
     <meta name="language" content="en">
 
     <meta http-equiv="content-language" content="en" />
-    <meta name="url" content="https://quotes.arthaud.dev">
-    <meta name="identifier-URL" content="https://quotes.arthaud.dev">
-    <link rel="canonical" href="https://quotes.arthaud.dev" />
+    <meta name="url" content="https://quoted.arthaud.dev">
+    <meta name="identifier-URL" content="https://quoted.arthaud.dev">
+    <link rel="canonical" href="https://quoted.arthaud.dev" />
 
     <meta name="subject" content="Quotes">
     <meta name="description" content="{{ $description }}" />
-    <meta name="keywords" content="quotes, litterature, arthaud, proust">
+    <meta name="keywords" content="quoted, litterature, arthaud, proust">
     <meta name="theme-color" content="#16161a">
 
     <meta property="og:title" content="{{ $title }}" />
     <meta property="og:type" content="website" />
     <meta property="og:description" content="{{ $description }}" />
-    <meta property="og:site_name" content="{{config('app.name', 'Quotes')}}" />
-    <meta property="og:url" content="https://quotes.arthaud.dev" />
+    <!-- <meta property="og:site_name" content="{{config('app.name', 'Quotes')}}" /> -->
+    <meta property="og:url" content="https://quoted.arthaud.dev" />
     <meta property="og:locale" content="en" />
-    <meta property="og:image" content="https://quotes.arthaud.dev/img/hero.min.png" />
+    <meta property="og:image" content="https://quoted.arthaud.dev/img/hero.min.png" />
 
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:title" content="{{ $title }}" />
     <meta name="twitter:description" content="{{ $description }}" />
-    <meta name="twitter:site" content="https://quotes.arthaud.dev" />
-    <meta name="twitter:image" content="https://quotes.arthaud.dev/img/hero.min.png" />
+    <meta name="twitter:site" content="https://quoted.arthaud.dev" />
+    <meta name="twitter:image" content="https://quoted.arthaud.dev/img/hero.min.png" />
 
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-title" content="Quotes" />
@@ -70,7 +70,7 @@ $title = View::hasSection('title')?View::getSection('title').' - '.config('app.n
             "@context": "http://schema.org",
             "@type": "Organization",
             "name": "{{config('app.name', 'Quotes')}}",
-            "url": "https://quotes.arthaud.dev",
+            "url": "https://quoted.arthaud.dev",
             "address": "none",
             "sameAs": [
                 "",
