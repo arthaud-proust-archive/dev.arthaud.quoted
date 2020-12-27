506c99e6de9aa60ea4b4746a9532326316ebc986
Arthaud <proust@arthaud.dev>
2020-12-27 02:06:57 +0100

    Add quotesCount value

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,6 +17,7 @@ class HomeController extends ConfigController
     public function index(Request $request) {
         if(Auth::check() && Auth::user()->isAdmin()) {
             return view('home', [
+                "quotesCount" => Quote::count(),
                 "toverifQuotes" => Quote::unVerified()->get()->reverse(),
                 "philoQuotes" => Quote::where('group', 'philo')->verified()->get()->reverse()->take(3),
                 "lastQuotes" => Quote::all()->reverse()->take(3),
@@ -25,6 +26,7 @@ class HomeController extends ConfigController
             ]);
         } else {
             return view('home', [
+                "quotesCount" => Quote::count(),
                 "philoQuotes" => Quote::where('group', 'philo')->verified()->get()->reverse()->take(3),
                 "lastQuotes" => Quote::all()->reverse()->take(3),
                 "popularQuotes" => Quote::orderBy('views', 'desc')->get()->take(3),
